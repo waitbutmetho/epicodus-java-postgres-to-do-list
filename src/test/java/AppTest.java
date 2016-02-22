2c95ef4c0b034d8303bcba3e97d0f4300e3846bc
Josh <silversevenfox@yahoo.com>
2016-02-22 14:23:19 -0800

    to-do-list w/ postgres

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,68 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  // @Test
+  // public void rootTest() {
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("Todo list!");
+  // }
+  //
+  // @Test
+  // public void categoryIsCreatedTest() {
+  //   goTo("http://localhost:4567/");
+  //   click("a", withText("Add a new category"));
+  //   fill("#name").with("Household chores");
+  //   submit(".btn");
+  //   assertThat(pageSource()).contains("Your category has been saved.");
+  // }
+  //
+  // @Test
+  // public void categoryIsDisplayedTest() {
+  //   goTo("http://localhost:4567/categories/new");
+  //   fill("#name").with("Household chores");
+  //   submit(".btn");
+  //   click("a", withText("View categories"));
+  //   assertThat(pageSource()).contains("Household chores");
+  // }
+  //
+  // @Test
+  // public void categoryTasksFormIsDisplayed() {
+  //   goTo("http://localhost:4567/categories/new");
+  //   fill("#name").with("Shopping");
+  //   submit(".btn");
+  //   click("a", withText("View categories"));
+  //   click("a", withText("Shopping"));
+  //   click("a", withText("Add a new task"));
+  //   assertThat(pageSource()).contains("Add a Task to Shopping");
+  // }
+  //
+  // @Test
+  // public void tasksIsAddedAndDisplayed() {
+  //   goTo("http://localhost:4567/categories/new");
+  //   fill("#name").with("Banking");
+  //   submit(".btn");
+  //   click("a", withText("View categories"));
+  //   click("a", withText("Banking"));
+  //   click("a", withText("Add a new task"));
+  //   fill("#description").with("Deposit paycheck");
+  //   submit(".btn");
+  //   assertThat(pageSource()).contains("Deposit paycheck");
+  // }
+}
---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,55 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+
+
+public class TaskTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Task.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Task firstTask = new Task("Mow the lawn", 1);
+    Task secondTask = new Task("Mow the lawn", 1);
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAretheSame() {
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
+    assertTrue(Task.all().get(0).equals(myTask));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
+    Task savedTask = Task.all().get(0);
+    assertEquals(myTask.getId(), savedTask.getId());
+  }
+
+  @Test
+  public void find_findsTaskInDatabase_true() {
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
+    Task savedTask = Task.find(myTask.getId());
+    assertTrue(myTask.equals(savedTask));
+  }
+
+  @Test
+  public void save_savesCategoryIdIntoDB_true() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    Task myTask = new Task("Mow the lawn", myCategory.getId());
+    myTask.save();
+    Task savedTask = Task.find(myTask.getId());
+    assertEquals(savedTask.getCategoryId(), myCategory.getId());
+}
+}
